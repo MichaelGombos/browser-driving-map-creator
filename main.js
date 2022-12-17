cc3c6a5ac75d24af3386a26f8550bc565285d74c
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2022-12-17 09:56:56 -0600

    add spawn tile

--- main.js
+++ main.js
@@ -2,36 +2,57 @@ const rows = 20;
 const columns = 20;
 const tileTypes = [
   {  tileName:"road",    color:"orange",  colorHex:"#FFA500",  value:0},
+  {  tileName:"wall",    color: "red",    colorHex:"#FF0000",  value:1},
   {  tileName:"dirt",    color:"brown",   colorHex:"#964B00",  value:2},  
-  {  tileName:"wall",    color: "red",    colorHex:"#FF0000",  value:1}
+  {  tileName:"spawn",    color: "cyan",    colorHex:"71c9ff", value:3}
 ]
 const map = document.querySelector("#map");
 const paints = document.querySelector("#paints");
 const printButton = document.querySelector("#print");
 const result = document.querySelector("#result");
 let mapData = [];
-let currentFill = tileTypes[2] // wall
+let currentFill = tileTypes.find(t => t.tileName == "wall"); 
 let painting = false;
+let spawnTile =    
+{
+  type:tileTypes[3], //spawn
+  row:2,
+  column:2
+}
+let spawnElement;
 
+const fillTile = (e,currentFill) => {
+  //set selected tile to new type.
+  mapData[e.target.dataset.row][e.target.dataset.column].type = currentFill;
+
+  //remove all classes from selected tile 
+  e.target.classList.remove("road","dirt","wall","spawn");
+
+  if(currentFill == tileTypes.find(t => t.tileName == "spawn")){
+    //if spawn tile is already set, remove it from mapData, and change the dom map tile class to road
+    spawnElement.classList.remove("spawn");
+    spawnElement.classList.add("road");
+
+    //set spawn element again
+    spawnElement = e.target;
+  }
+
+  e.target.classList.add(currentFill.tileName); 
+}
 
 const handleTileFill = (method) => (e) => {
   if(method == "point"){
-    mapData[e.target.dataset.row][e.target.dataset.column].type = currentFill;
-    e.target.classList.remove("road","dirt","wall");
-    e.target.classList.add(currentFill.tileName); 
+    fillTile(e,currentFill);
   }
   else if(method == "drag"){
     //check if mouse is held down
     if(painting){
-      mapData[e.target.dataset.row][e.target.dataset.column].type = currentFill;
-      e.target.classList.remove("road","dirt","wall");
-      e.target.classList.add(currentFill.tileName); 
+      fillTile(e,currentFill);
     }
   }
 }
 
 const handleTypeChange = (type) => (e) => {
-  console.log("changed type");
   currentFill = type;
 }
 
@@ -44,7 +65,6 @@ const handlePrint = (e) => {
 //generate domMap and mapdata
 for(let r = 0; r < rows; r++ ){
   let mapRow = new Array();
-  console.log(mapRow);
   mapData.push(mapRow);
   let row = document.createElement("div");
   row.classList.add("row");
@@ -52,15 +72,25 @@ for(let r = 0; r < rows; r++ ){
     let tile = document.createElement("div")
     tile.dataset.row = r;
     tile.dataset.column = c;
-    mapRow.push(
-      {
-        type:tileTypes[0], //road
-        row:r,
-        column:c
-      }
-      );
-    tile.classList.add("tile");
-    tile.classList.add("road")
+    //place spawn tile
+    if(r == spawnTile.row && c == spawnTile.column){
+      mapRow.push(spawnTile);
+      tile.classList.add("tile");
+      tile.classList.add("spawn")
+      spawnElement = tile;
+    }
+    else{
+      mapRow.push(
+        {
+          type:tileTypes[0], //road
+          row:r,
+          column:c
+        });
+        tile.classList.add("tile");
+        tile.classList.add("road")
+    }
+
+
     tile.addEventListener("mouseover",handleTileFill("drag"))
     tile.addEventListener("click",handleTileFill("point"))
     row.appendChild(tile);
@@ -78,8 +108,6 @@ for(let type of tileTypes){
 
   paintTile.classList.add("paint-tile")
   paintTile.classList.add(type.tileName)
-  console.log(paintTile);
-  console.log(paintTile.style);
   paintTile.dataset.name = type.tileName;
   paintTile.addEventListener("click",handleTypeChange(type));
   
@@ -88,11 +116,6 @@ for(let type of tileTypes){
 
   paints.appendChild(paint);
 
-  console.log(type);
-  console.log(type.tileName);
-  console.log(type.color);
-  console.log(type.colorHex);
-  console.log(type.value);
 
   
 }
