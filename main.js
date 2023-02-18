4b68bb19aebc3377ae3f23d116a766e5319859c9
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2023-02-18 16:34:39 -0600

    update tileTypes to use old colors

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const tileTypes = [
-  {  tileName:"road",   value:0 , color: "#e69c69"},
-  {  tileName:"wall", value:1, color: "#8a4836"},
-  {  tileName:"dirt",  value:2, color : "#bf6f4a"},  
+  {  tileName:"road",   value:0 , color: "#FFA500"},
+  {  tileName:"wall", value:1, color: "#FF0000"},
+  {  tileName:"dirt",  value:2, color : "#964B00"},  
   {  tileName:"spawn", value:3, color: "#71c9ff"},
   { tileName:"finish-up", value:4, color: "#73ff71"},
   { tileName:"finish-down", value:5, color: "#ff29e2"},
   { tileName:"bumper",  value:6, color:  "#0027d2"},
-  { tileName:"check-point-left-road",  value:7, color: "#e69c69"},
-  { tileName:"check-point-right-road",  value:8, color: "#e69c69"},
-  { tileName:"check-point-left-dirt", value:9, color: "#bf6f4a"},
-  { tileName:"check-point-right-dirt", value:10 ,color: "#bf6f4a"},
+  { tileName:"check-point-left-road",  value:7, color: "rgb(78, 135, 162)"},
+  { tileName:"check-point-right-road",  value:8, color: "rgb(187, 187, 187)"},
+  { tileName:"check-point-left-dirt", value:9, color: "rgb(72, 183, 216)"},
+  { tileName:"check-point-right-dirt", value:10 ,color: "rgb(216, 216, 216)"},
 ]
 const mapCol = document.querySelector("#cols");
 const mapRow = document.querySelector("#rows");
